931f8fa36455ff74df99dc7a2553dbfdb22501fd
snowapril <sinjihng@naver.com>
2021-07-05 22:28:13 +0900

    feat : add ar core setup & session creation, camera permission acquire

--- app/src/main/java/com/sinbaram/mapgo/MainActivity.kt
+++ app/src/main/java/com/sinbaram/mapgo/MainActivity.kt
@@ -1,11 +1,111 @@
 package com.sinbaram.mapgo
 
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+import com.sinbaram.mapgo.AR.CameraPermissionHelper
 
 class MainActivity : AppCompatActivity() {
+    var mUserRequestedInstall = true
+    var mSession : Session? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!CheckARSupport())
+            Toast.makeText(this, "Cannot find AR Core module in this android device", Toast.LENGTH_LONG)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+            return
+        }
+
+        // Ensure that Google Play Services for AR and ARCore device profile data are
+        // installed and up to date.
+        try {
+            if (mSession == null) {
+                when (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    ArCoreApk.InstallStatus.INSTALLED -> {
+                        // Success: Safe to create the AR session.
+                        mSession = Session(this)
+
+                        // Create a session config.
+                        val config = Config(mSession)
+
+                        // Do feature-specific operations here, such as enabling depth or turning on
+                        // support for Augmented Faces.
+
+                        // Configure the session.
+                        mSession!!.configure(config)
+                    }
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                        // When this method returns `INSTALL_REQUESTED`:
+                        // 1. ARCore pauses this activity.
+                        // 2. ARCore prompts the user to install or update Google Play
+                        //    Services for AR (market://details?id=com.google.ar.core).
+                        // 3. ARCore downloads the latest device profile data.
+                        // 4. ARCore resumes this activity. The next invocation of
+                        //    requestInstall() will either return `INSTALLED` or throw an
+                        //    exception if the installation or update did not succeed.
+                        mUserRequestedInstall = false
+                        return
+                    }
+                }
+            }
+        } catch (e: UnavailableUserDeclinedInstallationException) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Release native heap memory used by an ARCore session.
+        mSession?.close()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, results)
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+    }
+
+    fun CheckARSupport() : Boolean {
+        val availability = ArCoreApk.getInstance().checkAvailability(this)
+        if (availability.isTransient) {
+            // Continue to query availability at 5Hz while compatibility is checked in the background.
+            Handler(Looper.getMainLooper()).postDelayed({
+                CheckARSupport()
+            }, 200)
+        }
+        return availability.isSupported
     }
 }
