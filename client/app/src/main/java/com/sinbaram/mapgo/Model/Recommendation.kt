8de2db7f9ea24305d5d9e35bb334b1c408580183
snowapril <sinjihng@naver.com>
2021-07-23 21:08:14 +0900

    feat(rest-api) : add recommendation data class
    implement data class for get response from server api

---
+++ client/app/src/main/java/com/sinbaram/mapgo/Model/Recommendation.kt
@@ -0,0 +1,10 @@
+package com.sinbaram.mapgo.Model
+
+import com.squareup.moshi.Json
+
+data class Recommendation(
+    @Json(name = "name") var name: String,
+    @Json(name = "longitude") var long: Float,
+    @Json(name = "lat") var latitude: Float,
+    @Json(name = "filtering") var filtering: Int,
+) {}
