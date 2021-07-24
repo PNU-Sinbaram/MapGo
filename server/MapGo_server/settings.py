5e4b69078633078188210786b3829fdab1eea71b
lijm1358 <lijm1358@gmail.com>
2021-07-24 16:11:17 +0900

    feat(server-restapi) : add django app checkin

--- server/MapGo_server/settings.py
+++ server/MapGo_server/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'checkin.apps.CheckinConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
