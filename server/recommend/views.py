70a7f15f6203932805730ec21aed1b2fdbe347a2
dldks321 <80047618+dldks321@users.noreply.github.com>
2021-07-25 16:58:28 +0900

    fix(server-restapi) : delete server/recommend directory

--- server/recommend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import PlaceDataSerializer, STCSerializer
-from .models import PlaceData, STC 
-
-# Create your views here.
-class STCViewSet(viewsets.ModelViewSet):
-    queryset = STC.objects.all()
-    serializer_class = STCSerializer
