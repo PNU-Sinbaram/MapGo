5d2905383dd33e89ad888828e96541e94c3af223
snowapril <sinjihng@naver.com>
2021-07-27 22:40:26 +0900

    feat(rest-api): rename CheckInLog class as CheckInResponse

---
+++ client/app/src/main/java/com/sinbaram/mapgo/Model/CheckInResponse.kt
@@ -0,0 +1,10 @@
+package com.sinbaram.mapgo.Model
+
+import com.squareup.moshi.Json
+
+data class CheckInResponse(
+    @Json(name = "User_ID") var userID: String,
+    @Json(name = "lat") var latitude: String,
+    @Json(name = "long") var longitude: String,
+    @Json(name = "timeStamp") var timeStamp: String
+) {}
