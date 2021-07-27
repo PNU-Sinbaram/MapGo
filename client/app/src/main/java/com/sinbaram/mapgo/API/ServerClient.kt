bab7508b88c11def5a9eb55df8fc53fa8362b4a8
snowapril <sinjihng@naver.com>
2021-07-27 22:42:21 +0900

    feat(rest-api): modify CheckIn post request parameter as Body

--- client/app/src/main/java/com/sinbaram/mapgo/API/ServerClient.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/API/ServerClient.kt
@@ -1,6 +1,7 @@
 package com.sinbaram.mapgo.API
 
-import com.sinbaram.mapgo.Model.CheckInLog
+import com.sinbaram.mapgo.Model.CheckInResponse
+import com.sinbaram.mapgo.Model.CheckInRequest
 import com.sinbaram.mapgo.Model.Recommendation
 import retrofit2.Call
 import retrofit2.http.*
@@ -8,16 +9,13 @@ import retrofit2.http.*
 interface ServerClient {
     @Headers("Accept: application/json")
     @POST("Mapgo/checkin/")
-    @FormUrlEncoded
     fun CheckIn(
-        @Field("User_ID") userID: String,
-        @Field("lat") latitude: Float,
-        @Field("long") longitude: Float
-    ): Call<CheckInLog>
+        @Body request: CheckInRequest
+    ): Call<CheckInResponse>
 
     @Headers("Accept: application/json")
     @GET("Mapgo/checkin/")
-    fun GetCheckIn(): Call<List<CheckInLog>>
+    fun GetCheckIn(): Call<List<CheckInResponse>>
 
     @Headers("Accept: application/json")
     @GET("Mapgo/recommend/")
---
+++ client/app/src/main/java/com/sinbaram/mapgo/Model/CheckInRequest.kt
@@ -0,0 +1,7 @@
+package com.sinbaram.mapgo.Model
+
+data class CheckInRequest(
+    var User_ID: String,
+    var lat: Float,
+    var long: Float
+) {}
