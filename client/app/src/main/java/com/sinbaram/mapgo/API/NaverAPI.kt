05dd5628b898447fb15cb6b7652667857ed3c0bf
snowapril <sinjihng@naver.com>
2021-08-01 21:22:07 +0900

    feat(rest-api) : add open api naver client
    as naver open api and ncloud api url connections are differed, add one more retrofit instance

--- client/app/src/main/java/com/sinbaram/mapgo/API/NaverAPI.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/API/NaverAPI.kt
@@ -4,15 +4,30 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object NaverAPI {
-    var BASE_URL: String = "https://naveropenapi.apigw.ntruss.com/"
-    var retrofit: Retrofit? = null
+    var NCLOUD_BASE_URL: String = "https://naveropenapi.apigw.ntruss.com/"
+    var nCloudRetrofit: Retrofit? = null
+    var OPENAPI_BASE_URL: String = "https://openapi.naver.com/"
+    var openApiRetrofit: Retrofit? = null
+
+    // Return ncloud naver api client
     fun GetClient(): Retrofit? {
-        if (retrofit == null) {
-            retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
+        if (nCloudRetrofit == null) {
+            nCloudRetrofit = Retrofit.Builder()
+                .baseUrl(NCLOUD_BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
         }
-        return retrofit
+        return nCloudRetrofit
+    }
+
+    // Return naver open api client
+    fun GetOpenAPIClient(): Retrofit? {
+        if (openApiRetrofit == null) {
+            openApiRetrofit = Retrofit.Builder()
+                .baseUrl(OPENAPI_BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+        }
+        return openApiRetrofit
     }
 }
