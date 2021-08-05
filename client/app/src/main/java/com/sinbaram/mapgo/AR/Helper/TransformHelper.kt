bd3377c478e4f832cca3b5efda52ac20db3a93db
snowapril <sinjihng@gmail.com>
2021-08-05 22:53:50 +0900

    feat(ar-maps) : implement bearing calculation function
    calculate bearing between two locations and return

---
+++ client/app/src/main/java/com/sinbaram/mapgo/AR/Helper/TransformHelper.kt
@@ -0,0 +1,24 @@
+package com.sinbaram.mapgo.AR.Helper
+
+import android.location.Location
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.ln
+import kotlin.math.PI
+import kotlin.math.tan
+
+class TransformHelper {
+    companion object {
+        fun calculateBearing(loc1 : Location, loc2 : Location) : Double {
+            val latA = loc1.latitude * PI / 180
+            val lonA = loc1.longitude * PI / 180
+            val latB = loc2.latitude * PI / 180
+            val lonB = loc2.longitude * PI / 180
+
+            val deltaOmega = ln(tan((latB / 2) + (PI / 4)) / tan((latA / 2) + (PI / 4)))
+            val deltaLongitude = abs(lonA - lonB)
+
+            return atan2(deltaLongitude, deltaOmega)
+        }
+    }
+}
