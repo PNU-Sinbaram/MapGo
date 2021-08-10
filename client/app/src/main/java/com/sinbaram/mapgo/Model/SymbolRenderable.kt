997b27324ddb284d8f2cfc1fed3acd876237dd77
snowapril <sinjihng@naver.com>
2021-08-10 20:37:11 +0900

    refactor(ar-maps) : add comments for resolve undocumented warnings

--- client/app/src/main/java/com/sinbaram/mapgo/Model/SymbolRenderable.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/Model/SymbolRenderable.kt
@@ -3,10 +3,12 @@ package com.sinbaram.mapgo.Model
 import com.google.ar.sceneform.AnchorNode
 import com.naver.maps.map.Symbol
 
+/** Symbol and AnchorNode Pair data class */
 data class SymbolRenderable(
     var symbol: Symbol,
     var anchor: AnchorNode?
 ) {
+    /** Compare only with symbol */
     override fun equals(other: Any?): Boolean {
         if (other !is SymbolRenderable) {
             return false
@@ -14,6 +16,7 @@ data class SymbolRenderable(
         return this.symbol == other.symbol
     }
 
+    /** Get hashcode using symbol */
     override fun hashCode(): Int {
         return this.symbol.hashCode()
     }
