60a66f621b1a961ca536519f6cad3918703a40a6
snowapril <sinjihng@naver.com>
2021-08-15 19:28:13 +0900

    fix(rest-api) : add missing module import

--- client/app/src/main/java/com/sinbaram/mapgo/API/SearchClient.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/API/SearchClient.kt
@@ -1,6 +1,7 @@
 package com.sinbaram.mapgo.API
 
 import com.sinbaram.mapgo.Model.BuildingQuery
+import com.sinbaram.mapgo.Model.ImageQuery
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
