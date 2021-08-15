ebda89ee934da10eb174aa2ae5bb3f8729b4b866
snowapril <sinjihng@naver.com>
2021-08-15 18:31:56 +0900

    refactor(rest-api) : fix linter issues

--- client/app/src/main/java/com/sinbaram/mapgo/API/ServerClient.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/API/ServerClient.kt
@@ -3,8 +3,12 @@ package com.sinbaram.mapgo.API
 import com.sinbaram.mapgo.Model.CheckInResponse
 import com.sinbaram.mapgo.Model.CheckInRequest
 import com.sinbaram.mapgo.Model.Recommendation
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Query
 import retrofit2.Call
-import retrofit2.http.*
 
 interface ServerClient {
     @Headers("Accept: application/json")
--- client/app/src/main/java/com/sinbaram/mapgo/Model/ImageModel.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/Model/ImageModel.kt
@@ -2,7 +2,7 @@ package com.sinbaram.mapgo.Model
 
 import com.squareup.moshi.Json
 
-data class ImageModel (
+data class ImageModel(
     @Json(name = "title") var name: String,
     @Json(name = "link") var linkUrl: String,
     @Json(name = "thumbnail") var thumbnail: String,
