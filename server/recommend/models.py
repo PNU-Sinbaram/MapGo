82b1eb8a2ef96eed4a5c69371ff9a59caa319958
lijm1358 <lijm1358@gmail.com>
2021-08-15 16:11:24 +0900

    feat(server-restapi) : add recommend model for request handling

--- server/recommend/models.py
+++ server/recommend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -14,3 +15,11 @@ class PlaceData(models.Model):
 class STC(models.Model):
     recommend1 = models.JSONField()
     recommend2 = models.JSONField()
+
+class PlaceRequestData(models.Model):
+    User_ID = models.CharField(max_length=100)
+    lat = models.FloatField(validators=[MinValueValidator(-85), MaxValueValidator(85)])
+    long = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
+    keywords = models.CharField(max_length=50)
+    epsilon = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
