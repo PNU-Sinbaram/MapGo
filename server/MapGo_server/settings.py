4dd6bc61f5abf3a73d1c71497851e04d88188ab6
lijm1358 <lijm1358@gmail.com>
2021-08-16 16:17:46 +0900

    fix(server-restapi) : remove exposure of Django key

--- server/MapGo_server/settings.py
+++ server/MapGo_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&4vgk2"
-"(j4=2b7+ypqoqnoet7uu_=4q=jicnbqz-j0qsx4e@2vw"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
