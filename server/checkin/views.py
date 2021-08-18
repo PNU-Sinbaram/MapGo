dedd7bf88c8b7264d63c28e7b64bc78640c51655
lijm1358 <lijm1358@gmail.com>
2021-08-18 18:21:31 +0900

    feat(server-recommend2) : add checkin request field placeName, placelist as checkin response
    Only positional value of location can't specify exact place name,
    so endpoint checkin now gets placeName as optional field.
    Also, it has place list as response if you don't specify place name as
    request.
    
    For further information, check server documentation.

--- server/checkin/views.py
+++ server/checkin/views.py
@@ -10,6 +10,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.core.exceptions import ObjectDoesNotExist
 
+import json
+import os
+import requests
+
 
 # Create your views here.
 @method_decorator(csrf_exempt, name='delete')
@@ -25,11 +29,39 @@ class CheckinViewSet(viewsets.ViewSet):
         requestData = JSONParser().parse(request)
         serializer = CheckinSerializer(data=requestData)
         if serializer.is_valid():
+            placeList = []
+
+            locationString = str(serializer.validated_data['lat'])+','+str(serializer.validated_data['long'])
+            googlePlaceApiKey = os.environ['GOOGLE_PLACES_KEY']
+            URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+            params = {'key': googlePlaceApiKey, 'location': locationString, 'radius': 10, 'language': 'ko'}
+            resp = json.loads(requests.get(URL, params=params).text)
+            resp = resp.get('results')
+
+            for place in resp:
+                if place["vicinity"] != place["name"]:
+                    placeList.append(place["vicinity"]+" "+place["name"])
+
             if serializer.validated_data.get('placeName', None) is None:
-                print("No placeName input.")
-                return Response("No placeName input.", status=200)
-            serializer.save()
-            return Response(serializer.data, status=201)
+                if len(placeList) == 0:
+                    print(resp)
+                    return Response("There are no places in "+locationString, status=400)
+                elif len(placeList) == 1:
+                    serializer.validated_data['placeName'] = placeList[0]
+                    serializer.save()
+                    return Response(serializer.data, status=200)
+                else:
+                    placeNameListJSON = json.dumps({"message": 'There are multiple location in requested position. Re-request with field "placeName"',
+                                                    "places": self.__locationListToJSON(placeList)}, ensure_ascii=False)
+                    print(placeNameListJSON)
+                    return Response(json.JSONDecoder().decode(placeNameListJSON), status=203)
+            else:
+                if serializer.validated_data.get('placeName') in placeList:
+                    serializer.save()
+                    return Response(serializer.data, status=200)
+                else:
+                    return Response("There is no place.", status=400)
+
         return Response(serializer.errors, status=400)
 
     @classmethod
@@ -41,3 +73,11 @@ class CheckinViewSet(viewsets.ViewSet):
             return Response("Delete : "+userid, status=200)
         except ObjectDoesNotExist:
             return Response("User "+userid+" not found.", status=400)
+
+    def __locationListToJSON(locationList):
+        locId = 1
+        placeNameList = []
+        for l in locationList:
+            placeNameList.append({'id': locId, 'placeName': l})
+            locId+=1
+        return placeNameList
