52c1def2f1cec481d209c978d18a91f7f2245d35
snowapril <sinjihng@naver.com>
2021-08-21 17:04:48 +0900

    feat(ar-recommend) : attach interceptor to retrofit2

--- client/app/src/main/java/com/sinbaram/mapgo/API/NaverAPI.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/API/NaverAPI.kt
@@ -1,5 +1,9 @@
 package com.sinbaram.mapgo.API
 
+import com.sinbaram.mapgo.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -13,8 +17,27 @@ object NaverAPI {
     // Return ncloud naver api client
     fun GetClient(): Retrofit? {
         if (nCloudRetrofit == null) {
+            val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("X-NCP-APIGW-API-KEY-ID", BuildConfig.NAVER_KEY_ID)
+                    .addHeader("X-NCP-APIGW-API-KEY", BuildConfig.NAVER_SECRET_KEY)
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
             nCloudRetrofit = Retrofit.Builder()
                 .baseUrl(NCLOUD_BASE_URL)
+                .client(client)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
         }
@@ -24,8 +47,27 @@ object NaverAPI {
     // Return naver open api client
     fun GetOpenAPIClient(): Retrofit? {
         if (openApiRetrofit == null) {
+            val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("X-Naver-Client-Id", BuildConfig.NAVER_OPEN_KEY_ID)
+                    .addHeader("X-Naver-Client-Secret", BuildConfig.NAVER_OPEN_SECRET_KEY)
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
             openApiRetrofit = Retrofit.Builder()
                 .baseUrl(OPENAPI_BASE_URL)
+                .client(client)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
         }
