bfdb383b49e53174e9f0295309f565adc49f2b7c
lijm1358 <lijm1358@gmail.com>
2021-08-24 02:15:42 +0900

    feat(server-sns) : add views for sns/user to upload image

--- server/sns/views.py
+++ server/sns/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 from .models import User
 
-
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+    def create(self, request):
+        requestData = {"userID": request.POST.get("userID"),
+                       "deviceID": request.POST.get("deviceID"),
+                       "username": request.POST.get("username"),
+                       "profileImage": request.FILES['profileImage']}
+        print(request.FILES['profileImage'])
+        print(requestData)
+        serializer = UserSerializer(data=requestData)
+        if serializer.is_valid(): 
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
