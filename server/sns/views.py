f6b679dc55d2cfc52b6f7594a55a187f0c56eb11
lijm1358 <lijm1358@gmail.com>
2021-08-23 23:32:25 +0900

    feat(server-sns) : add temporary view UserViewSet

--- server/sns/views.py
+++ server/sns/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .serializers import UserSerializer
+from .models import User
+
+
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
