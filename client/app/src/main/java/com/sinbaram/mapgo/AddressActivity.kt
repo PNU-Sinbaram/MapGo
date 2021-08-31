0de163311e411bd3d7a2eb7f49ee3516644eadcf
snowapril <sinjihng@gmail.com>
2021-08-31 19:06:02 +0900

    feat(ar-recommend) : separate address activity from navi activity

---
+++ client/app/src/main/java/com/sinbaram/mapgo/AddressActivity.kt
@@ -0,0 +1,51 @@
+package com.sinbaram.mapgo
+
+import android.content.Intent
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.sinbaram.mapgo.databinding.ActivityAddressBinding
+import com.sinbaram.mapgo.databinding.ActivityNavigationBinding
+
+class AddressActivity: AppCompatActivity() {
+    // Activity binding variable
+    private lateinit var mBinding: ActivityAddressBinding
+
+    private lateinit var mWebView: WebView
+
+    inner class JsInterface {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        fun processData(data: String) {
+            val extra = Bundle()
+            val intent = Intent()
+            extra.putString("data", data)
+            intent.putExtras(extra)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Binding mapgo activity layout
+        mBinding = ActivityAddressBinding.inflate(layoutInflater)
+
+        mWebView = mBinding.webView
+        mWebView.settings.javaScriptEnabled = true
+        mWebView.addJavascriptInterface(JsInterface(), "Android")
+        mWebView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                mWebView.loadUrl("javascript:sample2_execDaumPostcode();")
+            }
+        }
+
+        mWebView.loadUrl(BuildConfig.SERVER_ADDRESS + "/images/html/daum.html")
+
+        // Pass activity binding root
+        setContentView(mBinding.root)
+    }
+}
--- client/app/src/main/java/com/sinbaram/mapgo/NavigationActivity.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/NavigationActivity.kt
@@ -1,30 +1,35 @@
 package com.sinbaram.mapgo
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.MenuInflater
+import android.view.View
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.sinbaram.mapgo.Model.ProfileModel
 import com.sinbaram.mapgo.databinding.ActivityNavigationBinding
+import java.util.regex.Pattern
 
 class NavigationActivity: AppCompatActivity() {
     // Activity binding variable
     private lateinit var mBinding: ActivityNavigationBinding
 
-    private lateinit var mWebView: WebView
-
-    inner class JsInterface {
-        @JavascriptInterface
-        @SuppressWarnings("unused")
-        fun processData(data: String) {
-            val extra = Bundle()
-            val intent = Intent()
-            extra.putString("data", data)
-            intent.putExtras(extra)
-            setResult(RESULT_OK, intent)
-            finish()
+    //
+    private var mDestination: String? = null
+        get() {
+            return field
         }
+
+    companion object {
+        const val ADDRESS_ACTIVITY_CODE = 1470
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,18 +38,38 @@ class NavigationActivity: AppCompatActivity() {
         // Binding mapgo activity layout
         mBinding = ActivityNavigationBinding.inflate(layoutInflater)
 
-        mWebView = mBinding.webView
-        mWebView.settings.javaScriptEnabled = true
-        mWebView.addJavascriptInterface(JsInterface(), "Android")
-        mWebView.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                mWebView.loadUrl("javascript:sample2_execDaumPostcode();")
-            }
+        mBinding.addressButton.setOnClickListener {
+            val intent = Intent(applicationContext, AddressActivity::class.java)
+            overridePendingTransition(0, 0)
+            startActivityForResult(intent, ADDRESS_ACTIVITY_CODE)
         }
 
-        mWebView.loadUrl(BuildConfig.SERVER_ADDRESS + "/images/html/daum.html")
+        mBinding.confirmButton.setOnClickListener {
+            val resultIntent = Intent()
+            if (mDestination != null)
+                resultIntent.putExtra("Destination", mDestination)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
+
+        mBinding.cancelButton.setOnClickListener {
+            finish()
+        }
 
         // Pass activity binding root
         setContentView(mBinding.root)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val extras: Bundle = data!!.extras!!
+            when (requestCode) {
+                ADDRESS_ACTIVITY_CODE -> {
+                    mDestination = extras.getString("data")
+                    mBinding.addressButton.text = mDestination!!
+                }
+            }
+        }
     }
 }
