e6d2cb563313f63e4a37aeed94dc2b6793f89145
snowapril <sinjihng@gmail.com>
2021-08-31 16:02:01 +0900

    feat(ar-recommend) : set timeout to retrofit2

--- client/app/src/main/java/com/sinbaram/mapgo/API/ServerAPI.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/API/ServerAPI.kt
@@ -3,6 +3,9 @@ package com.sinbaram.mapgo.API
 import com.sinbaram.mapgo.BuildConfig
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
 
 /** Retrofit2 singleton instance for mapgo server */
 object ServerAPI {
@@ -10,9 +13,15 @@ object ServerAPI {
     var retrofit: Retrofit? = null
     fun GetClient(): Retrofit? {
         if (retrofit == null) {
+            val okHttpClient = OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.MINUTES)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(15, TimeUnit.SECONDS)
+                .build()
             retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
+                .client(okHttpClient)
                 .build()
         }
         return retrofit
