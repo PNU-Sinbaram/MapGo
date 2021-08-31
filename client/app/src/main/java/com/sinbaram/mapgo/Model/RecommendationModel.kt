7f9e173288f09b35ecdf5166771ed6c5e038ab50
snowapril <sinjihng@gmail.com>
2021-08-31 16:01:28 +0900

    feat(ar-recommend) : fix query field name

--- client/app/src/main/java/com/sinbaram/mapgo/Model/RecommendationModel.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/Model/RecommendationModel.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 /** mapgo server api recommendation response model */
 data class RecommendationModel(
     @Json(name = "name") var name: String,
-    @Json(name = "longitude") var long: Float,
-    @Json(name = "lat") var latitude: Float,
+    @Json(name = "lat") var lat: Float,
+    @Json(name = "long") var long: Float,
     @Json(name = "filtering") var filtering: Int,
 )
