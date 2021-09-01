4671be44e8cced0bab7f5a7462d641df965b3015
lijm1358 <lijm1358@gmail.com>
2021-09-01 20:03:46 +0900

    feat(snsfeed) : add post author on the sns feed

--- client/app/src/main/java/com/sinbaram/mapgo/SnsFeedActivity.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/SnsFeedActivity.kt
@@ -3,6 +3,9 @@ package com.sinbaram.mapgo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.sinbaram.mapgo.Model.PostFeedItem
 
 class SnsFeedActivity : AppCompatActivity() {
@@ -30,6 +33,14 @@ class SnsFeedActivity : AppCompatActivity() {
         for (imageobj in postInfo.postImage) {
             postImageList += baseurl + imageobj.image
         }
+
+        val userName = findViewById<TextView>(R.id.userName)
+        val userimage = findViewById<ImageView>(R.id.userImage)
+        val posttime = findViewById<TextView>(R.id.postTime)
+
+        userName.setText(writerName)
+        posttime.setText(postTime)
+        Glide.with(this).load(writerImage).into(userimage)
 
         //Log.d("outputtest", postId.toString()+" "+writerName+" "+writerImage+" "+postcontent+" "+postcontent+" "+postImageList+" "+postLat+" "+postLong+" "+postTime)
     }
