a64b64732d419f61d474d25a0080da0bfe8e2542
lijm1358 <lijm1358@gmail.com>
2021-09-01 18:11:47 +0900

    feat(snsfeed) : add activity to show sns feeds

---
+++ client/app/src/main/java/com/sinbaram/mapgo/SnsFeedActivity.kt
@@ -0,0 +1,11 @@
+package com.sinbaram.mapgo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SnsFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sns_feed)
+    }
+}
