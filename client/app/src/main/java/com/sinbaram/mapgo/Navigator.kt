7ec7ffb271658137b1972d61b0c01c844c5928cb
snowapril <sinjihng@gmail.com>
2021-09-01 11:14:21 +0900

    feat(ar-recommend) : add callback field for use queried direction path

--- client/app/src/main/java/com/sinbaram/mapgo/Navigator.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/Navigator.kt
@@ -2,6 +2,7 @@ package com.sinbaram.mapgo
 
 import android.location.Location
 import android.util.Log
+import com.naver.maps.map.Symbol
 import com.sinbaram.mapgo.API.DirectionClient
 import com.sinbaram.mapgo.API.GeocodingClient
 import com.sinbaram.mapgo.API.NaverAPI
@@ -10,16 +11,14 @@ import com.sinbaram.mapgo.Model.GeocodingModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.reflect.KFunction1
 
-class Navigator(source: Location, destination: String) {
-    // Naver Direction api response model
-    var mDirectionData: DirectionModel? = null
-        get() {
-            return field
-        }
+class Navigator(callback: KFunction1<DirectionModel, Unit>) {
+    // navigation direction callback
+    private var mNaviCallback: KFunction1<DirectionModel, Unit>
 
     init {
-        startNavigation(source, destination)
+        mNaviCallback = callback
     }
 
     fun startNavigation(source: Location, destination: String) {
@@ -47,7 +46,7 @@ class Navigator(source: Location, destination: String) {
         apiCall.enqueue(object: Callback<DirectionModel> {
             override fun onResponse(call: Call<DirectionModel>, response: Response<DirectionModel>) {
                 if (response.code() == 200) {
-                    mDirectionData = response.body()
+                    mNaviCallback(response.body()!!)
                 }
             }
             override fun onFailure(call: Call<DirectionModel>, t: Throwable) {
