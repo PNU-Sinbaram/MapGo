0199ba180561920c45e12c9bf96978b1dad1ede1
snowapril <sinjihng@gmail.com>
2021-09-01 12:08:56 +0900

    feat(ar-recommend) : add signedArea function

--- client/app/src/main/java/com/sinbaram/mapgo/AR/Helper/MathUtilHelper.kt
+++ client/app/src/main/java/com/sinbaram/mapgo/AR/Helper/MathUtilHelper.kt
@@ -1,13 +1,14 @@
 package com.sinbaram.mapgo.AR.Helper
 
 import android.location.Location
+import com.naver.maps.geometry.LatLng
 import java.lang.Math.toDegrees
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sin
 
 /** Collection of coordinates transformation utilities */
-class TransformHelper {
+class MathUtilHelper {
     companion object {
         /** Calculate angle between two locations which share same up vector */
         fun calculateBearing(loc1: Location, loc2: Location): Double {
@@ -20,6 +21,11 @@ class TransformHelper {
             val y = sin(lon) * cos(latB)
             val x = cos(latA) * sin(latB) - sin(latA) * cos(latB) * cos(lon)
             return toDegrees(atan2(y, x))
+        }
+
+        /** */
+        fun signedArea(pos1: LatLng, pos2: LatLng): Double {
+            return pos1.longitude * pos2.latitude - pos1.latitude * pos2.longitude
         }
     }
 }
